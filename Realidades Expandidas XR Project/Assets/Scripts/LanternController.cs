96d38512cc941b486f7860e164ced4d967ea289b
Juhxx <juliacosta232005@gmail.com>
2026-02-05 18:19:15 +0000

    Add Lantern Script and Base ENemy Scripts

---
+++ Realidades Expandidas XR Project/Assets/Scripts/LanternController.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LanternController : MonoBehaviour
+{
+    [SerializeField] private float _maxCharge;
+    [SerializeField] private float _chargeAmount;
+    [SerializeField] private float _chargeDecay;
+
+    [SerializeField] private float _currentCharge;
+
+    public float CurrentCharge {
+        get => _currentCharge;
+        set
+        {
+            if (value < 0) _currentCharge = 0;
+            else if (value > _maxCharge) _currentCharge = _maxCharge;
+            else _currentCharge = value;
+
+            UpdateLightIntensity();
+        }
+    }
+
+    [SerializeField] InputActionProperty _triggerAction;
+
+    [SerializeField] private Light _spotLight;
+    [SerializeField] private float _lightMaxIntensity;
+
+    [SerializeField] private bool _inUse = false;
+
+    public bool InUse {
+        get => _inUse; 
+        set
+        {
+            if (value == false) CurrentCharge = 0;
+
+            _inUse = value;
+        } 
+    }
+
+    private void Update()
+    {
+        if (_inUse)
+        {
+            DoDecay();
+            CheckCharge();
+        }
+    }
+
+    private void DoDecay()
+    {
+        CurrentCharge -= _chargeDecay;
+    }
+
+    private void DoCharge()
+    {
+        CurrentCharge += _chargeAmount;
+    }
+
+    private void CheckCharge()
+    {
+        if (_triggerAction.action.ReadValue<float>() > 0)
+        {
+            DoCharge();
+            Debug.Log("Winding");
+        }
+    }
+
+    private void UpdateLightIntensity()
+    {
+        float chargeRatio = _currentCharge/_maxCharge;
+
+        _spotLight.intensity = Mathf.Lerp(0, _lightMaxIntensity, chargeRatio);
+    }
+
+}
